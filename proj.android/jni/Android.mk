5ee5dfd2ec02f21ffb30c588f798bd8179c0acc5
smiger <530509325@qq.com>
2018-03-23 10:16:00 +0800

    android编译

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -9,13 +9,27 @@ $(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos/audio/include)
 
 LOCAL_MODULE := MyGame_shared
 
-LOCAL_MODULE_FILENAME := libMyGame
+#LOCAL_MODULE_FILENAME := libMyGame
 
-LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+#LOCAL_SRC_FILES := hellocpp/main.cpp \
+#                   ../../Classes/AppDelegate.cpp \
+#                   ../../Classes/HelloWorldScene.cpp
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+#LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+
+define walk
+    $(wildcard $(1)) $(foreach e, $(wildcard $(1)/*), $(call walk, $(e)))
+endef
+
+ALLFILES = $(call walk, $(LOCAL_PATH)/../../Classes)
+FILE_LIST := hellocpp/main.cpp
+
+FILE_LIST += $(filter %.cpp, $(ALLFILES))
+LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
+
+FILE_INCLUDES := $(filter-out %.cpp, $(ALLFILES))
+FILE_INCLUDES := $(filter-out %.h, $(FILE_INCLUDES))
+LOCAL_C_INCLUDES := $(FILE_INCLUDES)
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
